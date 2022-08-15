a5b20ef6d3ae0733d56a87f9a4544edaa1a59390
Mohamad Oghli <mhd.sh.oghli@gmail.com>
2022-08-15 15:15:06 +0400

    update app ui

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import urllib.request
 import urllib.error
 from collections import defaultdict
@@ -5,12 +6,12 @@ from collections import Counter
 import re
 
 
-def analyze_text(file_url):
+def analyze_text(file_src):
     '''
     Analyze text file data according to different measures
 
     :param
-    file_url(str): Valid download link of the input text file
+    file_url(str): Valid download link or file name of the input text file
 
     :return
     text(dict): Dictionary containing information of text analysis of the input file,
@@ -32,36 +33,56 @@ def analyze_text(file_url):
     avg_char_word = 0
     mw_size = 0
     n_char = 0
+    file_url = False
     s_words = defaultdict(lambda: 0, s_words)
     words = defaultdict(lambda: 0, words)
     chars = defaultdict(lambda: 0, chars)
     try:
         # for reading input file in the current directory
-        # file = open(file_url, encoding='utf-8')
-        # reading input file from download url
-        file = urllib.request.urlopen(file_url)
-        # validate if the requested file in correct format
-        header = file.info()
-        if header["content-type"] != "text/plain":
-            msg = "Sorry, the requested file content isn't .txt format."
-            return msg
+        if "http" not in file_src:
+            file = open(file_src, encoding='utf-8')
+        else:
+            # reading input file from download url
+            file = urllib.request.urlopen(file_src)
+            file_url = True
+            # validate if the requested file in correct format
+            header = file.info()
+            if header["content-type"] != "text/plain":
+                msg = "Sorry, the requested file content isn't .txt format."
+                return msg
         # perform file operations
-        for line in file:
-            line = line.decode('utf-8')
-            n_char += len(line)
-            line = re.sub(r'[^a-zA-Z]', ' ', line)
-            line = line.split()
-            for word in line:
-                if len(word) > 3:
-                    words[word] += 1
-                elif 1 < len(word) <= 3:
-                    s_words[word] += 1
-                n_letter += len(word)
-                for char in word:
-                    chars[char] += 1
-            n_word += len(line)
-            n_line += 1
-        #file.close()
+        if file_url:
+            for line in file:
+                line = line.decode('utf-8')
+                n_char += len(line)
+                line = re.sub(r'[^a-zA-Z]', ' ', line)
+                line = line.split()
+                for word in line:
+                    if len(word) > 3:
+                        words[word] += 1
+                    elif 1 < len(word) <= 3:
+                        s_words[word] += 1
+                    n_letter += len(word)
+                    for char in word:
+                        chars[char] += 1
+                n_word += len(line)
+                n_line += 1
+        else:
+            for line in file:
+                n_char += len(line)
+                line = re.sub(r'[^a-zA-Z]', ' ', line)
+                line = line.split()
+                for word in line:
+                    if len(word) > 3:
+                        words[word] += 1
+                    elif 1 < len(word) <= 3:
+                        s_words[word] += 1
+                    n_letter += len(word)
+                    for char in word:
+                        chars[char] += 1
+                n_word += len(line)
+                n_line += 1
+            file.close()
         if n_word > 0:
             avg_word_line = round(n_word / n_line, 2)
             avg_char_line = round(n_char / n_line, 2)
@@ -89,7 +110,7 @@ def analyze_text(file_url):
         return msg
 
     summary = f"""____________Summary______________
-File Link: {file_url}
+File Source: {file_src}
 Total words: {n_word}
 Total letters: {n_letter}
 Total lines: {n_line}
@@ -118,11 +139,34 @@ Estimate file size: {f_size} KB
     text['f_size'] = f_size
     return text
 
+def st_ui():
+    '''
+    Render the User Interface of the application endpoints
+    '''
+    st.title("Text File Analyzer")
+    st.header("Upload a text file to analyze its data")
+    file = st.file_uploader("Choose a file", type=['txt'], accept_multiple_files=False)
+    if file:
+        file_name = file.name
+        analyze_result = analyze_text(file_name)
+        if type(analyze_result) is dict:
+            summary = analyze_result['s']
+            st.subheader("Brief Information")
+            st.text(summary)
+            st.subheader("Detailed Information")
+            st.json(analyze_result)
+        else:
+            st.write(analyze_result)
+
 
 if __name__ == "__main__":
-    analyze_result = analyze_text('https://drive.google.com/uc?export=download&id=1r1Urz_92YixjegWvaW_6cVTJQvGCOmGk')
-    if type(analyze_result) is dict:
-        summary = analyze_result['s']
-        print(summary)
-    else:
-        print(analyze_result)
+    # render the app using streamlit ui function
+    st_ui()
+    # file_url = 'https://drive.google.com/uc?export=download&id=1r1Urz_92YixjegWvaW_6cVTJQvGCOmGk'
+    # file_name = 'sample.txt'
+    # analyze_result = analyze_text(file_name)
+    # if type(analyze_result) is dict:
+    #     summary = analyze_result['s']
+    #     print(summary)
+    # else:
+    #     print(analyze_result)
